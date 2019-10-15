515547c57840a09e7add325f2fbb8ca6d23f8282
Maria Pepelassis <maria.pepelassis@mail.utoronto.ca>
2019-10-15 11:41:30 -0400

    Problem 4

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -88,7 +88,9 @@ function returnArray() {
  * @param {} thisArray of minimum length 3
  * @returns {string} a sentence  composed from the first 3 elements of the array
  */
-function splitArray (thisArray) {
+function splitArray () {
+  thisArray= ['Tim', 'Horton', 'Donut'];
+  return thisArray[0] + " " + thisArray[1] + " " + "was a " + thisArray[2];
   // remember you can access individual elements of an array with
   // array[0]
   // array[1]
